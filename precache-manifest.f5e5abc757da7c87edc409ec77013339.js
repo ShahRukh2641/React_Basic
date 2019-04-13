a2c579cd97668a77c9dcd510cfab73651cdcc0bd
Shah Rukh <shahrukh2641@gmail.com>
2019-04-13 10:42:48 +0500

    Updates

---
+++ precache-manifest.f5e5abc757da7c87edc409ec77013339.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "188a8966624752af85fa",
+    "url": "/static/js/main.2451fe37.chunk.js"
+  },
+  {
+    "revision": "a674aae0c09fc3b2fcee",
+    "url": "/static/js/2.51bac2eb.chunk.js"
+  },
+  {
+    "revision": "188a8966624752af85fa",
+    "url": "/static/css/main.ca43751a.chunk.css"
+  },
+  {
+    "revision": "334750aa6796c58df1d456eddb99ae8e",
+    "url": "/index.html"
+  }
+];
